04c0086351190111b68858ce19d938c71694da07
Manuel Millón <72598519+ManuelMillon@users.noreply.github.com>
2020-11-13 23:41:58 +0100

    Add files via upload

---
+++ experimento1/packageexperimento1/HolaReme.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package packageexperimento1;
+import java.util.Scanner;
+/**
+ * @author Manolo Millón
+ *
+ */
+public class HolaReme {
+
+	/**
+	 * Hoy nos hemos encontrado todo un grupo del colegio.
+	 */
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("¿Dónde está Reme?");
+		System.out.println("Encuentra a Reme y gana un gallifante. ");
+		
+		System.out.print("Por favor, introduce el número de Reme: ");
+		double numero = teclado.nextDouble();
+		double telefono;
+		telefono = 666333999;
+		
+		
+		if ((numero) == (telefono)) {
+			System.out.println("Enhorabuena, has encontrado a Reme.");
+		} else {
+			System.out.println("Querido compañero del Ortega: Mala suerte, continúa buscando.");
+		}
+			
+		}
+
+	}
+
+
